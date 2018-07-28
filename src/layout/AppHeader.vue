cdb4cf368d9514bde06fc098222f543e3b095d48
cristij <joracristi@gmail.com>
2018-07-29 02:08:26 +0300

    Fix main link in header

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -2,9 +2,9 @@
     <header class="header-global">
         <nav id="navbar-main" class="navbar navbar-main navbar-expand-lg navbar-transparent navbar-light">
             <div class="container">
-                <a class="navbar-brand mr-lg-5" href="../index.html">
+                <router-link class="navbar-brand mr-lg-5" to="/">
                     <img src="img/brand/white.png">
-                </a>
+                </router-link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
