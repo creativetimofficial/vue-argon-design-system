69b71359ffbc93f52945a52dcae7c379e9ec4c4d
cristij <joracristi@gmail.com>
2018-07-31 23:37:50 +0300

    Add dropdown component Add active as active class for exact router link matches

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -17,50 +17,46 @@
             </div>
 
             <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link" data-toggle="dropdown" role="button">
+                <base-dropdown class="nav-item" menu-classes="dropdown-menu-xl">
+                    <a slot="title" href="#" class="nav-link" data-toggle="dropdown" role="button">
                         <i class="ni ni-ui-04 d-lg-none"></i>
                         <span class="nav-link-inner--text">Components</span>
                     </a>
-                    <div class="dropdown-menu dropdown-menu-xl">
-                        <div class="dropdown-menu-inner">
-                            <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
-                               class="media d-flex align-items-center">
-                                <div class="icon icon-shape bg-gradient-primary rounded-circle text-white">
-                                    <i class="ni ni-spaceship"></i>
-                                </div>
-                                <div class="media-body ml-3">
-                                    <h6 class="heading text-primary mb-md-1">Getting started</h6>
-                                    <p class="description d-none d-md-inline-block mb-0">Get started with Bootstrap, the
-                                        world's most popular framework for building responsive sites.</p>
-                                </div>
-                            </a>
-                            <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
-                               class="media d-flex align-items-center">
-                                <div class="icon icon-shape bg-gradient-warning rounded-circle text-white">
-                                    <i class="ni ni-ui-04"></i>
-                                </div>
-                                <div class="media-body ml-3">
-                                    <h5 class="heading text-warning mb-md-1">Components</h5>
-                                    <p class="description d-none d-md-inline-block mb-0">Learn how to use Argon
-                                        compiling Scss, change brand colors and more.</p>
-                                </div>
-                            </a>
-                        </div>
+                    <div class="dropdown-menu-inner">
+                        <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
+                           class="media d-flex align-items-center">
+                            <div class="icon icon-shape bg-gradient-primary rounded-circle text-white">
+                                <i class="ni ni-spaceship"></i>
+                            </div>
+                            <div class="media-body ml-3">
+                                <h6 class="heading text-primary mb-md-1">Getting started</h6>
+                                <p class="description d-none d-md-inline-block mb-0">Get started with Bootstrap, the
+                                    world's most popular framework for building responsive sites.</p>
+                            </div>
+                        </a>
+                        <a href="https://demos.creative-tim.com/vue-argon-design-system/documentation/"
+                           class="media d-flex align-items-center">
+                            <div class="icon icon-shape bg-gradient-warning rounded-circle text-white">
+                                <i class="ni ni-ui-04"></i>
+                            </div>
+                            <div class="media-body ml-3">
+                                <h5 class="heading text-warning mb-md-1">Components</h5>
+                                <p class="description d-none d-md-inline-block mb-0">Learn how to use Argon
+                                    compiling Scss, change brand colors and more.</p>
+                            </div>
+                        </a>
                     </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link" data-toggle="dropdown" role="button">
+                </base-dropdown>
+                <base-dropdown tag="li" class="nav-item">
+                    <a slot="title" href="#" class="nav-link" data-toggle="dropdown" role="button">
                         <i class="ni ni-collection d-lg-none"></i>
                         <span class="nav-link-inner--text">Examples</span>
                     </a>
-                    <div class="dropdown-menu">
-                        <router-link to="/landing" class="dropdown-item">Landing</router-link>
-                        <router-link to="/profile" class="dropdown-item">Profile</router-link>
-                        <router-link to="/login" class="dropdown-item">Login</router-link>
-                        <router-link to="/register" class="dropdown-item">Register</router-link>
-                    </div>
-                </li>
+                    <router-link to="/landing" class="dropdown-item">Landing</router-link>
+                    <router-link to="/profile" class="dropdown-item">Profile</router-link>
+                    <router-link to="/login" class="dropdown-item">Login</router-link>
+                    <router-link to="/register" class="dropdown-item">Register</router-link>
+                </base-dropdown>
             </ul>
             <ul class="navbar-nav align-items-lg-center ml-lg-auto">
                 <li class="nav-item">
@@ -106,12 +102,14 @@
 </template>
 <script>
 import BaseNav from "@/components/BaseNav";
+import BaseDropdown from "@/components/BaseDropdown";
 import CloseButton from "@/components/CloseButton";
 
 export default {
   components: {
     BaseNav,
-    CloseButton
+    CloseButton,
+    BaseDropdown
   }
 };
 </script>
