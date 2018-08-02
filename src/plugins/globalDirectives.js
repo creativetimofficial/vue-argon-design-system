7d458748bac07cc7351e205c7758888f0c676559
cristij <joracristi@gmail.com>
2018-08-02 22:20:59 +0300

    Remove click away directive and add custom click outside directive

--- src/plugins/globalDirectives.js
+++ src/plugins/globalDirectives.js
@@ -1,12 +1,13 @@
-import { directive } from "vue-clickaway";
+import clickOutside from '../directives/click-ouside.js';
+
 /**
  * You can register global directives here and use them as a plugin in your main Vue instance
  */
 
 const GlobalDirectives = {
-  install(Vue) {
-    Vue.directive("click-outside", directive);
+  install (Vue) {
+    Vue.directive('click-outside', clickOutside);
   }
-};
+}
 
-export default GlobalDirectives;
+export default GlobalDirectives
