d56e430c480fe871f60c715b0a00e8a05e4f0b60
cristij <joracristi@gmail.com>
2018-08-14 12:02:59 +0300

    Add different navigation menus like in the original HTML version

--- src/views/components/Navigation.vue
+++ src/views/components/Navigation.vue
@@ -7,14 +7,14 @@
             </h2>
         </div>
         <!-- Navbar default -->
-        <base-nav v-for="type in menuTypes"
-                   :key="type"
-                   :type="type"
-                   effect="dark"
-                   expand
-                   title="Default Color"
-                   class="mt-4"
-                   :content-id="`navbar-${type}`">
+        <base-nav v-for="menu in menus"
+                  :key="menu.type"
+                  :type="menu.type"
+                  effect="dark"
+                  expand
+                  title="Default Color"
+                  class="mt-4"
+                  :content-id="`navbar-${menu.type}`">
             <div class="row" slot="content-header" slot-scope="{closeMenu}">
                 <div class="col-6 collapse-brand">
                     <a href="./index.html">
@@ -22,58 +22,50 @@
                     </a>
                 </div>
                 <div class="col-6 collapse-close">
-                    <close-button @click="closeMenu" :target="`navbar-${type}`">
+                    <close-button @click="closeMenu" :target="`navbar-${menu.type}`">
 
                     </close-button>
                 </div>
             </div>
-
-            <ul class="navbar-nav ml-lg-auto">
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="#">
-                        <i class="ni ni-favourite-28"></i>
-                        <span class="nav-link-inner--text d-lg-none">Discover</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="#">
-                        <i class="ni ni-notification-70"></i>
-                        <span class="nav-link-inner--text d-lg-none">Profile</span>
-                    </a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link nav-link-icon" href="#" id="navbar-default_dropdown_1" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="ni ni-settings-gear-65"></i>
-                        <span class="nav-link-inner--text d-lg-none">Settings</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbar-default_dropdown_1">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-            </ul>
-
+            <component :is="menu.menuComponent"></component>
         </base-nav>
     </section>
 
 </template>
 <script>
-import BaseNav from "@/components/BaseNav";
-import CloseButton from "@/components/CloseButton";
+  import BaseNav from "@/components/BaseNav";
+  import CloseButton from "@/components/CloseButton";
+  import Menu1 from './Navigation/Menu1'
+  import Menu2 from './Navigation/Menu2'
+  import Menu3 from './Navigation/Menu3'
+  import Menu4 from './Navigation/Menu4'
+  import Menu5 from './Navigation/Menu5'
+  import Menu6 from './Navigation/Menu6'
 
-export default {
-  components: {
-    BaseNav,
-    CloseButton
-  },
-  data() {
-    return {
-      menuTypes: ["default", "primary", "success", "danger", "warning", "info"]
-    };
-  }
-};
+  export default {
+    components: {
+      BaseNav,
+      CloseButton,
+      Menu1,
+      Menu2,
+      Menu3,
+      Menu4,
+      Menu5,
+      Menu6
+    },
+    data() {
+      return {
+        menus: [
+          {type: "default", menuComponent: Menu1},
+          {type: "primary", menuComponent: Menu2},
+          {type: "success", menuComponent: Menu3},
+          {type: "danger", menuComponent: Menu4},
+          {type: "warning", menuComponent: Menu5},
+          {type: "info", menuComponent: Menu6},
+        ]
+      };
+    }
+  };
 </script>
 <style>
 </style>
---
+++ src/views/components/Navigation/Menu6.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#">
+                <i class="fa fa-facebook-square"></i>
+                <span class="nav-link-inner--text">Facebook</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#">
+                <i class="fa fa-twitter"></i>
+                <span class="nav-link-inner--text">Twitter</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link nav-link-icon" href="#">
+                <i class="fa fa-instagram"></i>
+                <span class="nav-link-inner--text">Instagram</span>
+            </a>
+        </li>
+    </ul>
+</template>
+<script>
+  export default {}
+</script>
+<style>
+</style>
