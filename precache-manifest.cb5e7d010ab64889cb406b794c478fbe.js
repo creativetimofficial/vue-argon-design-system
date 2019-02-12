b5accd7acc93aae43daaac62cb2da6170b88eceb
cristij <joracristi@gmail.com>
2019-02-12 21:43:59 +0200

    Updates

---
+++ precache-manifest.cb5e7d010ab64889cb406b794c478fbe.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/vue-argon-design-system/favicon.png"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/vue-argon-design-system/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "52369ac11f2e9c281323",
+    "url": "/vue-argon-design-system/js/chunk-52e4de86.96d44776.js"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/vue-argon-design-system/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "7b8f87f357be302e65c2",
+    "url": "/vue-argon-design-system/js/chunk-ebdaf33a.3cf4ae2a.js"
+  },
+  {
+    "revision": "a1ffc1af6b99e2d4bb0b",
+    "url": "/vue-argon-design-system/js/chunk-vendors.befedce2.js"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/vue-argon-design-system/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-argon-design-system/robots.txt"
+  },
+  {
+    "revision": "3161213a8afd535b3a72",
+    "url": "/vue-argon-design-system/js/app.4e464511.js"
+  },
+  {
+    "revision": "670f3ddac9fe53735e97b0ac2bf5a106",
+    "url": "/vue-argon-design-system/index.html"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/vue-argon-design-system/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/vue-argon-design-system/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/vue-argon-design-system/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/vue-argon-design-system/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/vue-argon-design-system/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/vue-argon-design-system/img/theme/landing.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/vue-argon-design-system/img/theme/profile.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/vue-argon-design-system/img/theme/promo-1.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vue-argon-design-system/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/vue-argon-design-system/img/brand/blue.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/vue-argon-design-system/img/brand/white.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/vue-argon-design-system/img/brand/favicon.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/vue-argon-design-system/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/vue-argon-design-system/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/vue-argon-design-system/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/vue-argon-design-system/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/vue-argon-design-system/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/vue-argon-design-system/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vue-argon-design-system/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vue-argon-design-system/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vue-argon-design-system/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vue-argon-design-system/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "7b8f87f357be302e65c2",
+    "url": "/vue-argon-design-system/css/chunk-ebdaf33a.47b8dc24.css"
+  },
+  {
+    "revision": "3161213a8afd535b3a72",
+    "url": "/vue-argon-design-system/css/app.b9e8726e.css"
+  }
+];
