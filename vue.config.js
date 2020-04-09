f6c40f8bae5930507fd1cb52dfc7377dfb826324
fg-uulm <florian.geiselhart@uni-ulm.de>
2020-04-09 15:57:42 +0200

    Dev server config for local oauth testing, oauth working

--- vue.config.js
+++ vue.config.js
@@ -19,5 +19,9 @@ module.exports = {
   css: {
     // Enable CSS source maps.
     sourceMap: process.env.NODE_ENV !== 'production'
+  },
+  devServer: {
+    disableHostCheck: true,
+    https: true
   }
 };
