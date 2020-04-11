6d842b4bbbbfaa09ee6e0f48c47d73484928fa29
fg-uulm <florian.geiselhart@uni-ulm.de>
2020-04-11 16:38:01 +0200

    reinsert api key

--- src/main.js
+++ src/main.js
@@ -31,6 +31,7 @@ import './registerServiceWorker';
 import store from './store'
 
 const config = {
+  key: 'AIzaSyDLASxmRzFM9QroycxD-MNfP0L1bwWx0Ec',
   clientId: '678304282895-j9do95s9dt0kvh4no3ng2ll1e82kcvt8.apps.googleusercontent.com',
   scope: 'profile email '+ 
           'https://www.googleapis.com/auth/calendar '+
