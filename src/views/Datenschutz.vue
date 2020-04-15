958735286f916095bd26f3a8d71ae5a37658148b
Benjamin Thomsen <info@benjaminthomsen.de>
2020-04-16 01:46:42 +0200

    datenschutz page

--- src/views/Datenschutz.vue
+++ src/views/Datenschutz.vue
@@ -1,31 +1,57 @@
 <template>
     <div>
-        <AppSubheader></AppSubheader>                      
+        <AppSubheader></AppSubheader>
         <section class="section section-lg pt-lg-0 mt-5">
             <div class="container">
                 <div class="row mb-lg">
-                    <div class="col-lg-4">
-                        <h2>Datenschutz</h2>
+                    <div class="col-lg-12">
+                        <h2>Datenschutzvereinbarung</h2>
                     </div>
-                    <div class="col-lg-8">
+                    <div class="col-lg-8 ml-auto mt-4">
                         <p>
-                            <strong>Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans.</strong>
+                            <strong>Hier erhälst du Hinweise zum Umgang mit verschiedenen Daten im Hochschulkontext aus verschiedenen Perspektiven.</strong>
                         </p>
                         <br />
-                        <p>Ein kleines Bächlein namens Duden fließt durch ihren Ort und versorgt sie mit den nötigen Regelialien. Es ist ein paradiesmatisches Land, in dem einem gebratene Satzteile in den Mund fliegen. Nicht einmal von der allmächtigen Interpunktion werden die Blindtexte beherrscht – ein geradezu unorthographisches Leben.</p>
+                        <p>Im folgenden Bereich erhälst du eine detailierte Auflistung unserer gespeicherten Daten. Bei Unklarheiten kannst du dich gerne an die Hochschule wenden.</p>
                     </div>
-
                 </div>
 
-                <div class="row mt-5">
-
-                    <DatenschutzCard :passDatenschutzCardContent="passDatenschutzCardContent" />
+                <div class="row mb-md">
+                    <div class="col-lg-12">
+                        <h2>Professoren, Lehrbeauftrage und Mitarbeiter</h2>
+                    </div>
+                    <!-- <div class="col-lg-8 ml-auto mt-4">
+                        <p>
+                            <strong>Hier erhälst du Hinweise zum Umgang mit verschiedenen Daten im Hochschulkontext aus verschiedenen Perspektiven.</strong>
+                        </p>
+                        <br />
+                        <p>Im folgenden Bereich erhälst du eine detailierte Auflistung unserer gespeicherten Daten. Bei Unklarheiten kannst du dich gerne an die Hochschule wenden.</p>
+                    </div> -->
                 </div>
 
-           
+                <div class="row">
+                    <DatenschutzCard :passDatenschutzContent="passDatenschutzContent1" />
+                </div>
+
             </div>
         </section>
 
+        <section class="section section-lg pt-lg-0">
+            <div class="container">
+        
+                <div class="row mb-md">
+                    <div class="col-lg-12">
+                        <h2>Studenten und Studentinnen</h2>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <DatenschutzCard :passDatenschutzContent="passDatenschutzContent2" />
+                </div>
+
+               
+            </div>
+        </section>
     </div>
 </template>
 
@@ -44,39 +70,88 @@ export default {
 
     data: function() {
         return {
-            passDatenschutzCardContent: [
+            passDatenschutzContent1: [
+                {   
+                    id: Math.random().toString(36).substring(7),
+                    title: "Personenangaben <br>mit Arbeitsbezug",
+                    ampel: "green",
+                    info:
+                        "Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans.",
+                    bsp: "<li>Prof.</li> <li>Max Mustermann</li> <li>Rektor-Klaus-Str. 100</li> <li>74597 Schwäbisch Gmünd</li> <li>max.mustermann@hfg.design</li>"
+                },
                 {
-                    title: "Personenbezogen",
-                    desc: "Mitarbeiter",
-                    info: "Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans."
+                    id: Math.random().toString(36).substring(7),
+                    title: "Private<br>Personenangaben",
+                    ampel: "yellow",
+                    info:
+                        "Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans.",
+                    bsp: "<li>Prof.</li> <li>Max Mustermann</li> <li>Rektor-Klaus-Str. 100</li> <li>74597 Schwäbisch Gmünd</li> <li>Tel: 07171/xxx xxx</li> <li>max.mustermann@hfg.design</li>"
+                },
+                {
+                    id: Math.random().toString(36).substring(7),
+                    title: "Vertrauliche<br>Personenangaben",
+                    ampel: "red",
+                    info:
+                        "Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans.",
+                    bsp: "<li>Prof.</li> <li>Max Mustermann</li> <li>Rektor-Klaus-Str. 100</li> <li>74597 Schwäbisch Gmünd</li> <li>Tel: 07171/xxx xxx</li> <li>in ärtzlicher Behandlung</li> <li>max.mustermann@hfg.design</li>"
+                }
+            ],
+            passDatenschutzContent2: [
+                {
+                    id: Math.random().toString(36).substring(7),
+                    title: "Ohne<br>Personenangaben",
+                    ampel: "green",
+                    info:
+                        "Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans."
+                },
+                {
+                    id: Math.random().toString(36).substring(7),
+                    title: "Mit<br>Personenangaben",
+                    ampel: "yellow",
+                    info:
+                        "Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans."
+                },
+                {
+                    id: Math.random().toString(36).substring(7),
+                    title: "Vertrauliche<br>Personenangaben",
+                    ampel: "red",
+                    info:
+                        "Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte. Abgeschieden wohnen sie in Buchstabhausen an der Küste des Semantik, eines großen Sprachozeans."
                 }
             ],
             feedbackMessage: "",
             feedbackWasSent: false,
             feedbackConfirmation: "Sende Feedback...",
-            errorModal: false,
+            errorModal: false
         };
-    }, 
+    },
     methods: {
-        sendFeedback: function () {
+        sendFeedback: function() {
             this.feedbackWasSent = true;
-            this.$http.post("https://76i3an3137.execute-api.eu-central-1.amazonaws.com/prod/slackbridge/feedback", JSON.stringify({
-                text: this.feedbackMessage,
-            }),{
-                headers: {
-                    'Accept': 'application/json, text/plain, */*',
-                },
-            })
-            .then(result => {
-                    this.feedbackConfirmation = "Wir haben dein Feedback erhalten - vielen Dank!";
-            })
-            .catch(error => {
+            this.$http
+                .post(
+                    "https://76i3an3137.execute-api.eu-central-1.amazonaws.com/prod/slackbridge/feedback",
+                    JSON.stringify({
+                        text: this.feedbackMessage
+                    }),
+                    {
+                        headers: {
+                            Accept: "application/json, text/plain, */*"
+                        }
+                    }
+                )
+                .then(result => {
+                    this.feedbackConfirmation =
+                        "Wir haben dein Feedback erhalten - vielen Dank!";
+                })
+                .catch(error => {
                     // handle error
-                    this.feedbackConfirmation = "Leider ist etwas beim Versenden deines Feedbacks schief gegangen, bitte wende dich an einen der oben genannten Supportkontakte!";
+                    this.feedbackConfirmation =
+                        "Leider ist etwas beim Versenden deines Feedbacks schief gegangen, bitte wende dich an einen der oben genannten Supportkontakte!";
                     this.errorModal = true;
                     this.feedbackWasSent = false;
                     console.log(error);
-            })                            
+                });
         }
     }
 };
