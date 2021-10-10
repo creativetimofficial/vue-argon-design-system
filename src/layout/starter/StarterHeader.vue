2eff2e9fcd82fa05ce55c951ec3f03d447a758ba
sigridqqq <sigrid.qiu.shichao@gmail.com>
2021-10-10 18:05:44 +0800

    Change landing page

--- src/layout/starter/StarterHeader.vue
+++ src/layout/starter/StarterHeader.vue
@@ -2,7 +2,7 @@
     <header class="header-global">
         <base-nav class="navbar-main" transparent type="" effect="light" expand>
             <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
-                <img src="img/brand/white.png">
+                <img src="img/brand/scdf_logo_transparent.png" height="500" contain>
             </router-link>
 
             <div class="row" slot="content-header" slot-scope="{closeMenu}">
--- src/views/components/Hero.vue
+++ src/views/components/Hero.vue
@@ -16,38 +16,7 @@
             <div class="col px-0">
                 <div class="row justify-content-center align-items-center">
                     <div class="col-lg-7 text-center pt-lg">
-                        <img src="img/brand/white.png" style="width: 200px;" class="img-fluid">
-                        <p class="lead text-white mt-4 mb-5">A beautiful Design System for Bootstrap 4. It's Free and Open Source.</p>
-                        <div class="btn-wrapper">
-                            <base-button tag="a"
-                                         href="https://demos.creative-tim.com/vue-argon-design-system/documentation"
-                                         class="mb-3 mb-sm-0"
-                                         type="info"
-                                         icon="fa fa-code">
-                                Components
-                            </base-button>
-                            <base-button tag="a"
-                                         href="https://www.creative-tim.com/product/vue-argon-design-system"
-                                         class="mb-3 mb-sm-0"
-                                         type="white"
-                                         icon="ni ni-cloud-download-95">
-                                Download Vue
-                            </base-button>
-                        </div>
-                    </div>
-                </div>
-                <div class="row align-items-center justify-content-around stars-and-coded">
-                    <div class="col-sm-4">
-                        <span class="text-white alpha-7 ml-3">Star us on</span>
-                        <a href="https://github.com/creativetimofficial/argon-design-system" target="_blank" title="Support us on Github">
-                            <img src="img/brand/github-white-slim.png" style="height: 22px; margin-top: -3px">
-                        </a>
-                    </div>
-                    <div class="col-sm-4 mt-4 mt-sm-0 text-right">
-                        <span class="text-white alpha-7">Coded by</span>
-                        <a href="https://www.creative-tim.com" target="_blank" title="Creative Tim - Premium Bootstrap Themes and Templates">
-                            <img src="img/brand/creativetim-white-slim.png" class="ml-3" style="height: 30px;">
-                        </a>
+                        <h1 class="text-white mt-5 mb-6"><span>Session Case Notes</span></h1>
                     </div>
                 </div>
             </div>
