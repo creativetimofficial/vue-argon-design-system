808f95bc63d8a72a33c7c765a41bd778380452bd
amandatwr <amandatan2524@gmail.com>
2021-10-11 21:49:21 +0800

    Added in the open-ended questions + submit button

--- src/views/components/Hero.vue
+++ src/views/components/Hero.vue
@@ -49,11 +49,21 @@
                         <div class="text-center mt-5">
                             <h3>First Session Form</h3>
                         </div>
-                        <div class="mt-5 py-5 border-top text-center">
+                        <div class="mt-5 py-5 border-top text-left">
                             <div class="row justify-content-center">
                                 <div>
-                                    <h5>COUNSELLING GOALS</h5>
+                                    <h5><u>Counselling Goals</u></h5>
                                     <p>List down client’s counselling/ therapy goals (mutually agreed) to be achieved for counselling and/or during the current session.</p>
+                                    <textarea name="textarea" v-model.lazy.trim="textAreaValue" row=10 cols=110></textarea>
+                                    <br></br>
+
+                                    <h5><u>Case Conceptualisation</u></h5>
+                                    <p>Your assessment of the client’s key underlying issues, obstacles that prevent him / her from resolving the issues, factors that have contributed to his / her progress, personal strengths that could be leveraged to help client manage his / her issues better, perceived social support network to help client manage presenting problem(s), any suspected psychological conditions that the client may be presenting, etc.</p>
+                                    <textarea name="textarea" v-model.lazy.trim="textAreaValue" row=10 cols=110></textarea>
+                                    <br></br>
+
+                                    <h5><u>Intervention(s) provided</u></h5>
+                                    <p><em>Brief but clear summary of the intervention work that has been undertaken with the client during the session (e.g., what were the proposed plans that were agreed upon, any homework assigned to the client, etc.). If client expressed suicidal ideation, include suicide safety plan.</em></p>
                                     <textarea name="textarea" v-model.lazy.trim="textAreaValue" row=10 cols=110></textarea>
                                     <br></br>
 
